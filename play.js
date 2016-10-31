d24cfbe9d374d506684b2a98899ab63c1ec0c844
Emily Zhang <e.zhang@berkeley.edu>
2016-10-31 01:34:53 -0700

    refactor code

--- play.js
+++ play.js
@@ -3,58 +3,64 @@ var readlineSync = require('readline-sync');
 var sb = require('./scoreboard.js');
 var tictactoe = require('./tictactoe.js');
 
-// Play a game.
+// Play.
 function play() {
-    var mark, gridPos, winner, quit, player, game;
+    var quit;
     var scoreboard = sb.createScoreBoard();
     var player1First = true;
     console.log('Let\'s play tictactoe!\nINSTRUCTIONS: \nWhen it is ' +
                 'your turn, select a spot by choosing a number from 1 to 9!');
   
     while (true) {
-        game = tictactoe.newGame();
-        winner = undefined;
-        for (var i = 0; i < 9; i++) {
-            if (player1First) {
-                player = i;
-            } else {
-                player = i + 1;
-            }
-            console.log(tictactoe.gameToString(game));
-            if (player % 2 === 0) {
-                mark = 'X';
-                console.log('\nPlayer 1:');
-            } else {
-                mark = 'O';
-                console.log('\nPlayer 2:');
-            }
-            makeMove(game, mark);
-            game[gridPos] = mark;
-            winner = tictactoe.done(game);
-            if (winner == 'X') {
-                console.log('\nPlayer 1 wins!');
-                sb.keepScore(scoreboard, mark);
-                break;
-            } else if (winner == 'O') {
-                console.log('\nPlayer 2 wins!');
-                sb.keepScore(scoreboard, mark);
-                break;
-            }
-        }
-        console.log(tictactoe.gameToString(game));
-        if (winner === undefined) {
-            console.log('This game ended in a draw.');
-        }
+        playRound(player1First, scoreboard);
         console.log(sb.scoreboardToString(scoreboard));
         quit = readlineSync.question('\n\nDo you wish to quit the game ' +
                                         'now? Type \'q\' to quit.\n-> ');
         if (quit == 'q' || quit == 'Q') {
             break;
         }
-        console.log('\n\nNEW GAME:');
         player1First = !player1First;
     }
     end(scoreboard);
+}
+
+// Play a round of the game.
+function playRound(player1First, scoreboard) {
+    console.log('\n\nNEW GAME:');
+    var game = tictactoe.newGame();
+    var player, gridPos, mark;
+    var winner = undefined;
+    for (var i = 0; i < 9; i++) {
+        if (player1First) {
+            player = i;
+        } else {
+            player = i + 1;
+        }
+        console.log(tictactoe.gameToString(game));
+        if (player % 2 === 0) {
+            mark = 'X';
+            console.log('\nPlayer 1:');
+        } else {
+            mark = 'O';
+            console.log('\nPlayer 2:');
+        }
+        makeMove(game, mark);
+        game[gridPos] = mark;
+        winner = tictactoe.done(game);
+        if (winner == 'X') {
+            console.log('\nPlayer 1 wins!');
+            sb.keepScore(scoreboard, mark);
+            break;
+        } else if (winner == 'O') {
+            console.log('\nPlayer 2 wins!');
+            sb.keepScore(scoreboard, mark);
+            break;
+        }
+    }
+    console.log(tictactoe.gameToString(game));
+    if (winner === undefined) {
+        console.log('This game ended in a draw.');
+    }
 }
 
 // Get move from command line input.
